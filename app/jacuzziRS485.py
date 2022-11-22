1023193206868cb8f666e4f68000cb50bc1bb519
Jack Brown <jackbro@cisco.com>
2022-11-22 12:15:58 +0000

    Remove debug code

--- app/jacuzziRS485.py
+++ app/jacuzziRS485.py
@@ -394,7 +394,6 @@ class JacuzziRS485(BalboaSpaWifi):
         TEMP_FIELD_1 = 3  # Divide by 2 if in C, otherwise F
         self.tempField = data[3]
         self.curtemp = temp_convert[data[3]]
-        print(str(self.tempField))
 
         HEATER_FIELD_1 = 10  # = 64 when Heat on
         HEATER_SHIFT_1 = 6  # b1000000 when Heat on
